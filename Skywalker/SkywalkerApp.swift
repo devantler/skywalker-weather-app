91f3876889e77df8b6cfa6b1f6129c454a2c05dd
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2021-10-26 17:36:46 +0200

    Initial Commit

---
+++ Skywalker/SkywalkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkywalkerApp.swift
+//  Skywalker
+//
+//  Created by Nikolai Emil Damm on 26/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct SkywalkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
